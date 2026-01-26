9261ee67ce06a9751471316dbf68daf99174a5a6
developer-rafah <developer.rafah@gmail.com>
2026-01-26 19:03:16 +0300

    Update push.js

--- public/assets/js/push.js
+++ public/assets/js/push.js
@@ -77,3 +77,5 @@ export async function getPushStatus() {
   return Notification.permission === "granted" ? "مفعل ✅" :
          Notification.permission === "denied"  ? "مرفوض ❌" : "غير مفعل";
 }
+
+
